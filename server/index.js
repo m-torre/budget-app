3bdb7222bbfbf362aed2aa6d3dbde54aa50fd406
Maximiliano Torre <m.torre164@gmail.com>
2022-03-30 12:02:59 -0300

    Change build directory

--- server/index.js
+++ server/index.js
@@ -8,18 +8,18 @@ const { connectToDatabase } = require('./utils/db')
 
 const transactionsRouter = require('./controllers/transactions')
 
-app.use(express.static('build'))
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 app.use(express.json())
 app.use(cors())
 
 app.use('/api/transactions', transactionsRouter)
 
 app.get('/transactions', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'Unknown endpoint' })
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'Unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
